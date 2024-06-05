91bb598223ad28e4a00dd9ec23ed947637bbf022
nicolaics <nicolai.christian.s@gmail.com>
2024-06-05 19:23:15 +0900

    add code comment and remove if main

--- plotting.py
+++ plotting.py
@@ -6,16 +6,17 @@ import matplotlib.pyplot as plt
 
 def plot(accuracy: dict):
     plt.figure(figsize=(12, 8))
+
+    # plot the values of the accuracy
     plt.plot(accuracy.keys(), accuracy.values(), marker='o', linestyle='-')
 
     plt.xlabel('Training Data Set (%)')
     plt.ylabel('Accuracy')
 
+    # set the label at each point
     for i, (xi, yi) in enumerate(zip(accuracy.keys(), accuracy.values())):
         plt.annotate(f'({xi:.0f}%, {yi})', (xi, yi), textcoords="offset points",
                      xytext=(0, 8), ha='center')
+    
+    # show the line chart
     plt.show()
-
-if __name__ == "__main__":
-    accuracy = {10: 0.2, 30: 0.4, 50: 0.5, 70: 0.6, 100: 0.8}
-    plot(accuracy)
