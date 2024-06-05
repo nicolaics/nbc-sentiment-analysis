9cf7e168c923931b22a2a1c1cc45852a1013c157
nicolaics <nicolai.christian.s@gmail.com>
2024-06-06 00:10:23 +0900

    add code comment and remove if main

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,7 @@ import re
 
 from collections import defaultdict
 
+# convert the row data from the csv file
 def convert_row(headers, row):
     header_item_dictionary = {}
 
@@ -15,6 +16,7 @@ def convert_row(headers, row):
 
     return header_item_dictionary
 
+# get stopwords
 def get_stopwords() -> list:
     file_name = "stopwords.txt"
 
@@ -26,40 +28,51 @@ def get_stopwords() -> list:
 
     return stopwords
 
+# main driver to pre-process the data
 def preprocess(file_path, data_use=1) -> tuple[list, dict, dict]:
+    # open and read the csv file
     fh = open(file_path, "r")
 
     csv_reader = csv.reader(fh)
     headers = next(csv_reader)
 
-    train_data = []
+    data = []
 
     for row in csv_reader:
         item_dictionary = convert_row(headers, row)
-        train_data.append(item_dictionary)
+        data.append(item_dictionary)
 
     fh.close()
 
     stopwords = get_stopwords()
 
+    # variable to store all the word features
     word_features = defaultdict(int)
 
+    # total data's labels count
     data_labels_count = {'pos': 0, 'neg': 0}
 
     data_count = 0
-    target_data_count = data_use * len(train_data)
 
-    for row in train_data:
+    # number of training/test data to be utilize
+    target_data_count = round(data_use * len(data))
+
+    # pre-process the data
+    for row in data:
         text = row['text']
+
+        # lowercase the text
         text = text.lower()
         
+        # remove special chars
         text = re.sub('[!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~]+', ' ', text)
         
         label = int(row['stars'].strip())
 
+        # if the sentiment is positive sentiment
         if label == 5:
             data_labels_count['pos'] += 1
-        else:
+        else: # negative sentiment
             data_labels_count['neg'] += 1
 
         split_text = text.split()
@@ -71,8 +84,11 @@ def preprocess(file_path, data_use=1) -> tuple[list, dict, dict]:
 
             if (word != '') and (word not in stopwords):
                 word_features[word] += 1
+
+                # save the pre-process word in the list
                 cleaned_word.append(word)
 
+        # change the text into the pre-processed words list
         row['text'] = cleaned_word
 
         data_count += 1
@@ -80,8 +96,4 @@ def preprocess(file_path, data_use=1) -> tuple[list, dict, dict]:
         if data_count == target_data_count:
             break
 
-    return (train_data, data_labels_count, word_features)
-
-if __name__ == "__main__":
-    train_data_path = "./data/train.csv"
-    preprocess(train_data_path)
+    return (data, data_labels_count, word_features)
